91955d92919afeaf9c124c3b6a1b84916d3f3396
KonstantinosDiamantoudis <116115681+KonstantinosDiamantoudis@users.noreply.github.com>
2024-01-05 20:01:52 +0200

    Update UploadServlet.java

--- paradoteo2/UploadServlet.java
+++ paradoteo2/UploadServlet.java
@@ -1,23 +1,22 @@
 package paradoteo2;
 
 import java.io.*;
-import java.sql.Date;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.List;
 
 
-@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, // 2MB
-                 maxFileSize = 1024 * 1024 * 10,      // 10MB
-                 maxRequestSize = 1024 * 1024 * 50)   // 50MB
-
-public class UploadServlet extends HttpServlet implements Part {
+public class UploadServlet extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+      try {
         HttpSession session = request.getSession(true);
         User user = (User) session.getAttribute("authenticatedUser");
-        int userID = user.getUserId();
+        int userId = user.getUserId();
 
         String petKind = request.getParameter("petKind");
         int petAge = Integer.parseInt(request.getParameter("petAge"));
@@ -32,42 +31,32 @@ public class UploadServlet extends HttpServlet implements Part {
         boolean altered = "yes".equals(request.getParameter("altered"));
         String petDescription = request.getParameter("petDescription");
 
-        // Handling image upload
-        Part filePart = request.getPart("petPhoto");
-        String fileName = extractFileName(filePart);
-
-        // Save the image file in the "images" directory
+        InputStream inputStream = request.getInputStream();
         String uploadPath = getServletContext().getRealPath("") + File.separator + "images";
-        File uploadDir = new File(uploadPath);
-        if (!uploadDir.exists()) {
-            uploadDir.mkdir();
-        }
+
+        String timestamp = new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date());
+        String fileName = "uploadedFile_" + timestamp + ".jpg";
         String filePath = uploadPath + File.separator + fileName;
-        filePart.write(filePath);
 
-        String photo = fileName;
+        try (OutputStream outputStream = new FileOutputStream(new File(filePath))) {
+            int read;
+            byte[] bytes = new byte[1024];
 
-        try {
-
-        Post post = new Post();
-        post.createPost(userID, petKind, petAge, petWeight, petGender, petName, petColor,
-                location, vaccinated, chronicDisease, chipped, altered, photo, petDescription);
-
-        response.sendRedirect("home.jsp");
-
-        } catch (Exception e) {
-           // TODO: handle exception
-        }
-    }
-
-    private String extractFileName(Part part) {
-        String contentDisp = part.getHeader("content-disposition");
-        String[] tokens = contentDisp.split(";");
-        for (String token : tokens) {
-            if (token.trim().startsWith("filename")) {
-                return token.substring(token.indexOf("=") + 2, token.length() - 1);
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
             }
         }
-        return "";
+
+        Post post = new Post();
+        post.createPost(userId, petKind, petAge, petWeight, petGender, petName, petColor, location,
+                vaccinated, chronicDisease, chipped, altered, fileName, petDescription);
+
+        response.sendRedirect("home.jsp");
+      } catch (Exception e) {
+        
+        e.printStackTrace();
+       
+      }
+
     }
 }
